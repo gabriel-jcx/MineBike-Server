1f3882af01b6060e9049dc5fd63ba0f049d88c43
Gabriel Ji <gabrieljcx@hotmail.com>
2020-02-07 16:51:53 -0800

    added the expanded forge MOD based code to the repo

---
+++ forge-1.12.2-14.23.5.2838-mdk/src/main/java/edu/ics/uci/minebike/spongeplugin/SpongePlugin.java
@@ -0,0 +1,23 @@
+//package edu.ics.uci.minebike.spongeplugin;
+////import org.sp
+////import org.spongepowered.api.plugin.Plugin;
+////import org.spongepowered.api.event.*;
+////import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+////import org.spongepowered.api.plugin.Plugin;
+//
+////import org.spongepowered.api.plugin.*;
+//import javax.inject.Inject;
+//import org.slf4j.Logger;
+//import org.spongepowered.api.plugin.*;
+//
+//@Plugin(id = "minebike-server", name="SpongePlugin", version="1.0",description = "this is the very first sponge plugin")
+//public class SpongePlugin {
+//    @Inject
+//    private Logger logger;
+//
+////    @Listener
+////    public void onServerStart(GameStartedServerEvent event){
+////        System.out.println("Sponge Plugin Started");
+////        logger.info("Successfully ran My Plugin");
+////    }
+//}
