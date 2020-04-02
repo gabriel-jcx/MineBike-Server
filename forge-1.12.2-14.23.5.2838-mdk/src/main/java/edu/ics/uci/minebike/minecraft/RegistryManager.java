ecfc8280bd9e3a4f3e042228082c3e48b1bd684c
qiaor1 <39285530+qiaor1@users.noreply.github.com>
2020-04-01 17:36:47 -0700

    Fishingrod

---
+++ forge-1.12.2-14.23.5.2838-mdk/src/main/java/edu/ics/uci/minebike/minecraft/RegistryManager.java
@@ -0,0 +1,41 @@
+package edu.ics.uci.minebike.minecraft;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+import net.minecraftforge.event.RegistryEvent.Register;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import edu.ics.uci.minebike.minecraft.client.ItemManager;
+
+public class RegistryManager {
+    public RegistryManager() {
+    }
+
+    @EventBusSubscriber
+    public static class RegistryHandler {
+        public static SoundEvent FISH_SPLASHING_EVENT;
+        public static SoundEvent REELING_EVENT;
+
+        public RegistryHandler() {
+        }
+
+//        @SubscribeEvent
+//        public static void onRegisterBlock(Register<Block> e) {
+
+//        }
+
+        @SubscribeEvent
+        public static void onRegisterItem(Register<Item> e) {
+
+            ItemManager.register(e.getRegistry());
+        }
+
+//        @SubscribeEvent
+//        public static void onRegisterSound(Register<SoundEvent> e) {
+////
+//        }
+    }
+}
