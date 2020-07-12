f7c331dfb3fcafd6f8957900d21c6277a962db2c
qiaor1 <39285530+qiaor1@users.noreply.github.com>
2020-07-12 13:04:39 -0700

    outerAI

--- forge-1.12.2-14.23.5.2838-mdk/src/main/java/edu/ics/uci/minebike/minecraft/client/hud/OuterAIHud.java
+++ forge-1.12.2-14.23.5.2838-mdk/src/main/java/edu/ics/uci/minebike/minecraft/client/hud/OuterAIHud.java
@@ -9,6 +9,10 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import org.ngs.bigx.minecraft.BiGX;
+import org.ngs.bigx.minecraft.bike.BiGXPacketHandler;
+import org.ngs.bigx.minecraft.context.BigxClientContext;
+
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,11 +20,12 @@ public class OuterAIHud {
     public HudString heartString;
     private int hr=0;
     public HudString goalString;
+    public HudString goalReached;
     private int minGoal=0;
     private int maxGoal=150;
 
     public HudString minString;
-    private int minCount=0;
+    private int minCount=90;
     private int secCounter=0;
 
     public HudString incline;
@@ -37,24 +42,43 @@ public class OuterAIHud {
 
     }
     public void show_heart(){
+        System.out.println(((BigxClientContext) BiGX.instance().clientContext).heartrate);
 //        System.out.println(heartLocation+".........................................");
         Minecraft mc = Minecraft.getMinecraft();
         mc.getTextureManager().bindTexture(heartLocation);
 //        mc.currentScreen.drawTexturedModalRect(10,10,0,0,40,40);
+
+
         mc.ingameGUI.drawTexturedModalRect(0,0,0,0,40,40);
+        mc.ingameGUI.drawTexturedModalRect(5,40,40,5,25,80);
+        mc.ingameGUI.drawTexturedModalRect(5,40+minCount,70,5+minCount,25,80);
+
+    }
+    public void show_progress(){
 
     }
     public void show(){
-        this.heartString= new HudString(10,10, ""+hr,false,false);
-        this.goalString= new HudString(50, 10, "Heart Rate Goal:"+minGoal+"-"+maxGoal, false, false);
-        this.minString = new HudString(400, 10, "Time Played: "+minCount+"m", false, false);
-        this.incline = new HudString(10, 400, "Current working level: "+ level, false, false);
+        this.heartString= new HudString(14,16, ""+hr,false,false);
+        this.goalString= new HudString(50, 16, "Heart Rate Goal: "+minGoal+"-"+maxGoal, false, false);
+//        this.minString = new HudString(400, 10, "Time Played: "+minCount+"m", false, false);
+//        this.incline = new HudString(10, 400, "Current working level: "+ level, false, false);
     }
     public void refresh(){
+        if(minCount<=0){
+            this.goalReached= new HudString(10, 220, "Goal Reached!",false,false);
+        }
         if (current_t != (int) TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis())) {
             current_t = (int) TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis());
             hr+=1;
             heartString.text= ""+hr;
+            System.out.println(minCount);
+            if(minCount<=0){
+//                this.goalReached= new HudString(10, 220, "Goal Reached!",false,false);
+            }
+            else{
+                minCount-=10;
+            }
+//            show_heart();
 
         }
     }
