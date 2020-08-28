f6c04497bf819c6ee643b8e43205946039087dd5
Gabriel Ji <gabrieljcx@hotmail.com>
2020-08-27 23:40:40 -0700

    merging outerai

--- forge-1.12.2-14.23.5.2838-mdk/src/main/java/edu/ics/uci/minebike/minecraft/client/AI/OuterAI.java
+++ forge-1.12.2-14.23.5.2838-mdk/src/main/java/edu/ics/uci/minebike/minecraft/client/AI/OuterAI.java
@@ -6,6 +6,7 @@ import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import org.lwjgl.input.Keyboard;
+import org.ngs.bigx.minecraft.BiGX;
 
 import java.util.*;
 import java.util.concurrent.TimeUnit;
@@ -18,7 +19,7 @@ import java.util.concurrent.TimeUnit;
 //import org.ngs.bigx.minecraft.bike.BiGXPacketHandler;
 
 // should outer AI update QuestHeartRate
-public class OuterAI extends QuestHeartRate{
+public class OuterAI {
 
     //Stores the quest/mini games' world dimension and their avg heart rate for that game
     public Map<Integer, Integer> questList;
@@ -39,28 +40,53 @@ public class OuterAI extends QuestHeartRate{
     private boolean keyXPressed= false;
 
     private boolean popUpHudShowing= true;
+    // local instance
+    private static OuterAI instance = null;
 
-
+    // bike vars
+    private int currHR; // current heart rate
+    private int currResistance = 0; // current resistance level
+    private int prevSec;
 
     //Assign mini game AI to this variable
     public QuestHeartRate running_game;
     //Check the whether there is a mini game running or not
-    public QuestStatus questStatus=QuestStatus.NONE;
+    public QuestStatus questStatus;
 
-    public OuterAI(){
-
+    private OuterAI(){
+        questStatus = QuestStatus.NONE;
     }
 
-    public Integer getCurrent_heart_rate() {
-        return current_heart_rate;
+    public static OuterAI getInstance(){
+        if(instance == null)
+            instance = new OuterAI();
+        return instance;
     }
 
-    //Check whether the heart rate reach the goal
+    public int get_currHR() {
+        return this.currHR;
+    }
+
+    public void set_currHR(int heartRate){
+        this.currHR = heartRate;
+    }
+
+
+    private void updateHR(){
+        this.currHR = BiGX.instance().clientContext.heartrate;
+    }
+    // Update the heart rate / resistance value every second
     @SubscribeEvent
     public void onPlayerTick(TickEvent.PlayerTickEvent event) {
         //kids just started the game, don't modify
 
-        Integer curr = (int) TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis());
+        int currSec = (int) TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis());
+        if(currSec != prevSec){  // update every second
+            updateHR();
+            prevSec = currSec;
+        }
+
+
         if (questStatus==QuestStatus.NONE) {
             if (time != curr) {
                 time = curr;
